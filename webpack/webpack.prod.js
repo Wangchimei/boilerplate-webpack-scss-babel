b60793225df8ecd8f1e863f9f390181c29b7b7f3
Chi Mei Wang <kimmy0929@gmail.com>
2020-03-07 21:11:15 +0900

    revise command

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const common = require('./webpack.common');
 const merge = require('webpack-merge');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,6 +9,7 @@ module.exports = merge(common, {
     path: path.resolve('dist'),
     filename: '[contentHash].bundle.js',
   },
+  plugins: [new CleanWebpackPlugin()],
   module: {
     rules: [],
   },
