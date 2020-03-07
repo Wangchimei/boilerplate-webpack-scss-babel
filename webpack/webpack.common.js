f9d5a17b526ee70d23097843a8e369ba7c46cce6
Chi Mei Wang <kimmy0929@gmail.com>
2020-03-07 22:15:36 +0900

    able to minify html

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,22 +1,10 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: {
     main: './src/index.js',
     vendor: './src/vendor.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/template.html',
-    }),
-  ],
   module: {
     rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
       {
         test: /\.html$/i,
         loader: 'html-loader',
